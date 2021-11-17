313371b6a6ed4420510c1b694d91bf604245f582
Shrey Patel <shreyrg14@gmail.com>
2021-11-17 19:36:59 +0000

    added gitignore

--- parser/parse.py
+++ parser/parse.py
@@ -115,3 +115,10 @@ def parse(s):
 objectified = parse('((A+B)+C)+D')
 objectified.cluster()
 print(objectified.show())
+
+# 1. De Morgans
+# 2. Identity, idempotent, union, intersection, double negation
+# 3. associative, commutative
+# 4. apply absorbtion
+# 5. distributive property
+
--- parser/test2.py
+++
@@ -1,8 +0,0 @@
-# 1. De Morgans
-# 2. Identity, idempotent, union, intersection, double negation
-# 3. associative, commutative
-# 4. apply absorbtion
-# 5. distributive property
-
-
-
