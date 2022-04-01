4771abd1f4972e02720d677d14fc79a66da19941
Shrey Patel <shreyrg14@gmail.com>
2022-04-01 21:19:13 +0100

    added repr for expressions

--- parsing/tokens.py
+++ parsing/tokens.py
@@ -2,10 +2,11 @@ from functools import reduce
 from copy import deepcopy
 from itertools import product as distributeList
 
+
 class Expression:
     def __init__(self, *args):
         self.terms = list(args)
-        self.oldRep = ''
+        self.oldRep = ""
 
     def __eq__(self, other):
         if isinstance(other, type(self)):
@@ -20,19 +21,19 @@ class Expression:
 
     @property
     def rep(self):
-        return ''.join([i.rep for i in self.terms])
+        return "".join([i.rep for i in self.terms])
 
     def getLatex(self):
         if isinstance(self, Product):
             terms = [i.getLatex() for i in self.terms]
-            return ''.join(terms)
-            #return '%20\\cdot%20'.join(terms)
+            return "".join(terms)
+            # return '%20\\cdot%20'.join(terms)
         elif isinstance(self, Sum):
             terms = [i.getLatex() for i in self.terms]
-            return '\\left(' + '+'.join(terms) + '\\right)'
+            return "\\left(" + "+".join(terms) + "\\right)"
         elif isinstance(self, Not):
             term = self.term.getLatex()
-            return f'\\overline{{{term}}}'
+            return f"\\overline{{{term}}}"
         elif isinstance(self, Var):
             return self.term
         elif isinstance(self, Expression):
@@ -47,15 +48,18 @@ class Expression:
                 except:
                     print(term)
 
-    def repList(self):
-        print([i.rep for i in self.terms])
-        for term in self.terms:
-            if not isinstance(term, Var):
-                try:
-                    term.typeList()
-                except:
-                    print(term)
-                
+    def __repr__(self):
+        if isinstance(self, Sum):
+            return f'Sum({", ".join([term.repList() for term in self.terms])})'
+        elif isinstance(self, Product):
+            return f'Product({", ".join([term.repList() for term in self.terms])})'
+        elif isinstance(self, Not):
+            return f'Not({", ".join([term.repList() for term in self.terms])})'
+        elif isinstance(self, Var):
+            return f"Var({self.terms[0]})"
+        else:
+            return f'Expression({", ".join([term.repList() for term in self.terms])})'
+
     def getNot(self, obj):
         t = Expression(Not(obj))
         t.involution()
@@ -67,7 +71,6 @@ class Expression:
             if isinstance(term, (Sum, Product)) and len(term.terms) == 1:
                 self.terms.remove(term)
                 self.terms += term.terms
-            
 
     def testLaw(self, name, law, printed, last):
         law()
@@ -77,61 +80,69 @@ class Expression:
             out = [name, deepcopy(self)]
             last = deepcopy(self)
             return True, last, out
-        return printed, last, None 
+        return printed, last, None
 
     def simplify(self):
         self.cluster()
         self.unitize()
-        outs = [['Original', deepcopy(self)]]
+        outs = [["Original", deepcopy(self)]]
         last = deepcopy(self)
         printed = True
         while printed:
-            printed=False
-            
-            printed, last, out = self.testLaw('Associative', self.cluster, printed, last)
+            printed = False
+
+            printed, last, out = self.testLaw(
+                "Associative", self.cluster, printed, last
+            )
             if printed:
                 outs.append(out)
                 continue
-            printed, last, out = self.testLaw('Not', self.flip, printed, last)
+            printed, last, out = self.testLaw("Not", self.flip, printed, last)
             if printed:
                 outs.append(out)
                 continue
-            printed, last, out = self.testLaw('Null', self.null, printed, last)
+            printed, last, out = self.testLaw("Null", self.null, printed, last)
             if printed:
                 outs.append(out)
                 continue
-            printed, last, out = self.testLaw('Identity', self.identity, printed, last)
+            printed, last, out = self.testLaw("Identity", self.identity, printed, last)
             if printed:
                 outs.append(out)
                 continue
-            printed, last, out = self.testLaw('Idempotent', self.unique, printed, last)
+            printed, last, out = self.testLaw("Idempotent", self.unique, printed, last)
             if printed:
                 outs.append(out)
                 continue
-            printed, last, out = self.testLaw('Involution', self.involution, printed, last)
-            if printed:
-                outs.append(out)
-                continue            
-            printed, last, out = self.testLaw('Inverse', self.inverse, printed, last)
+            printed, last, out = self.testLaw(
+                "Involution", self.involution, printed, last
+            )
             if printed:
                 outs.append(out)
                 continue
-            printed, last, out = self.testLaw('De Morgans', self.deMorgans, printed, last)
+            printed, last, out = self.testLaw("Inverse", self.inverse, printed, last)
             if printed:
                 outs.append(out)
-                continue            
-            printed, last, out = self.testLaw('Absorption', self.absorb, printed, last)
+                continue
+            printed, last, out = self.testLaw(
+                "De Morgans", self.deMorgans, printed, last
+            )
             if printed:
                 outs.append(out)
-                continue 
-            printed, last, out = self.testLaw('Distributive', self.distribute, printed, last)
+                continue
+            printed, last, out = self.testLaw("Absorption", self.absorb, printed, last)
             if printed:
                 outs.append(out)
-                continue 
-            outs.append(['', deepcopy(self)])
+                continue
+            printed, last, out = self.testLaw(
+                "Distributive", self.distribute, printed, last
+            )
+            if printed:
+                outs.append(out)
+                continue
+            outs.append(["", deepcopy(self)])
         self.unitize()
         outs[-1][1] = self
-        return outs      
+        return outs
 
     def flip(self):
         for term in self.terms:
@@ -143,7 +154,8 @@ class Expression:
                     self.terms.remove(term)
                     self.terms.append(VAR0)
             term.unitize()
-            if not isinstance(term, Var): term.flip()
+            if not isinstance(term, Var):
+                term.flip()
 
     def inverse(self):
         for term in self.terms:
@@ -162,7 +174,8 @@ class Expression:
                         self.terms.append(VAR1)
                         break
             term.unitize()
-            if not isinstance(term, Var): term.inverse()
+            if not isinstance(term, Var):
+                term.inverse()
 
     def involution(self):
         for term in self.terms:
@@ -171,7 +184,8 @@ class Expression:
                     self.terms.remove(term)
                     self.terms.append(term.term.term)
             term.unitize()
-            if not isinstance(term, Var): term.involution()
+            if not isinstance(term, Var):
+                term.involution()
 
     def deMorgans(self):
         for term in self.terms:
@@ -184,7 +198,8 @@ class Expression:
                     self.terms.remove(term)
                     self.terms.append(Product(*[Not(i) for i in nterm.terms]))
             term.unitize()
-            if not isinstance(term, Var): term.deMorgans()
+            if not isinstance(term, Var):
+                term.deMorgans()
 
     def cluster(self):
         for term in self.terms:
@@ -194,7 +209,8 @@ class Expression:
                     if isinstance(sterm, Sum):
                         ind = tmp.index(sterm)
                         tmp.remove(sterm)
-                        for i in sterm.terms[::-1]: tmp.insert(ind, i)
+                        for i in sterm.terms[::-1]:
+                            tmp.insert(ind, i)
                 term.terms = tmp.copy()
             elif isinstance(term, Product):
                 tmp = term.terms.copy()
@@ -202,17 +218,21 @@ class Expression:
                     if isinstance(sterm, Product):
                         ind = tmp.index(sterm)
                         tmp.remove(sterm)
-                        for i in sterm.terms[::-1]: tmp.insert(ind, i)
+                        for i in sterm.terms[::-1]:
+                            tmp.insert(ind, i)
                 term.terms = tmp.copy()
             term.unitize()
-            if not isinstance(term, Var): term.cluster()
+            if not isinstance(term, Var):
+                term.cluster()
 
     def unique(self):
         tmp = []
         for term in self.terms:
-            if term not in tmp: tmp.append(term)
+            if term not in tmp:
+                tmp.append(term)
             term.unitize()
-            if not isinstance(term, Var): term.unique()
+            if not isinstance(term, Var):
+                term.unique()
         self.terms = tmp
 
     def identity(self):
@@ -223,9 +243,10 @@ class Expression:
             elif isinstance(term, Sum):
                 if VAR0 in term.terms and len(term.terms) > 1:
                     term.terms.remove(VAR0)
-                    
+
             term.unitize()
-            if not isinstance(term, Var): term.identity()
+            if not isinstance(term, Var):
+                term.identity()
 
     def null(self):
         for term in self.terms:
@@ -238,7 +259,8 @@ class Expression:
                     self.terms.remove(term)
                     self.terms.append(VAR1)
             term.unitize()
-            if not isinstance(term, Var): term.null()
+            if not isinstance(term, Var):
+                term.null()
 
     def absorb(self):
         for term in self.terms:
@@ -249,7 +271,9 @@ class Expression:
                         notPterms = [self.getNot(i) for i in pterms]
                         rest = [i for i in term.terms if i != sterm]
                         for every in rest:
-                            compareWith = every.terms if isinstance(every, Product) else [every]
+                            compareWith = (
+                                every.terms if isinstance(every, Product) else [every]
+                            )
                             match = [i for i in pterms if i in compareWith]
                             diff = [i for i in pterms if i not in compareWith]
                             notmatch = [i for i in notPterms if i in compareWith]
@@ -258,16 +282,17 @@ class Expression:
                                 self.terms.remove(term)
                                 extra = [i for i in rest if i != every]
                                 self.terms.append(Sum(Product(*match), *extra))
-                                return 
+                                return
 
                             if notmatch == compareWith:
                                 self.terms.remove(term)
                                 extra = [i for i in rest if i != every]
                                 self.terms.append(Sum(*notmatch, *diff, *extra))
                                 return
-                            
+
             term.unitize()
-            if not isinstance(term, Var): term.absorb()
+            if not isinstance(term, Var):
+                term.absorb()
 
     def distribute(self):
         for term in self.terms:
@@ -284,9 +309,10 @@ class Expression:
                     prod = list(distributeList(*lov))
                     fin = Sum(*[Product(*i) for i in prod])
                     self.terms.remove(term)
-                    self.terms.append(fin) 
+                    self.terms.append(fin)
             term.unitize()
-            if not isinstance(term, Var): term.distribute()
+            if not isinstance(term, Var):
+                term.distribute()
 
 
 class Product(Expression):
@@ -295,7 +321,8 @@ class Product(Expression):
 
     @property
     def rep(self):
-        return '(' + '*'.join([i.rep for i in self.terms]) + ')'
+        return "(" + "*".join([i.rep for i in self.terms]) + ")"
+
 
 class Sum(Expression):
     def __init__(self, *args):
@@ -303,13 +330,14 @@ class Sum(Expression):
 
     @property
     def rep(self):
-        return '(' + '+'.join([i.rep for i in self.terms]) + ')'
+        return "(" + "+".join([i.rep for i in self.terms]) + ")"
+
 
 class Not(Expression):
     def __init__(self, *args):
         super().__init__(*args)
         if len(self.terms) > 1:
-            raise ValueError(f'Not object has too many terms: {self.terms}')
+            raise ValueError(f"Not object has too many terms: {self.terms}")
 
     @property
     def rep(self):
@@ -319,22 +347,21 @@ class Not(Expression):
     def term(self):
         return self.terms[0]
 
+
 class Var(Expression):
     def __init__(self, *args):
         super().__init__(*args)
         if len(self.terms) > 1:
-            raise ValueError(f'Var object has too many terms: {self.terms}')
+            raise ValueError(f"Var object has too many terms: {self.terms}")
 
     @property
     def rep(self):
-        return self.term 
+        return self.term
 
     @property
     def term(self):
-        return self.terms[0]   
+        return self.terms[0]
 
 
-VAR0 = Var('0')
-VAR1 = Var('1')
-
-
+VAR0 = Var("0")
+VAR1 = Var("1")
